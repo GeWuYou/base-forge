7254e60a328c4a2aa822b23885fc2170e0a9471a
gewuyou <1063891901@qq.com>
2024-12-03 17:00:53 +0800

    refactor(develop): 重构代码
    提交信息：
    重构代码，更新了多个应用程序启动类及引用的基础类，将其路径从 `com.gewuyou.shared.app` 更改为 `com.gewuyou.web.app`。增加了详细的注释，并在多个类中添加了启动类的描述。新增了国际化响应信息的枚举类 `WebResponseInformation`，并更新了多个业务逻辑相关的异常、响应体及拦截器代码，改用新的响应信息。添加了资源文件，提高了系统的可用性和用户体验，同时新增了所有模块的启动横幅文件并统一格式。

--- base-forge-scheduler/src/main/java/com/gewuyou/scheduler/BaseForgeSchedulerApplication.java
+++ base-forge-scheduler/src/main/java/com/gewuyou/scheduler/BaseForgeSchedulerApplication.java
@@ -5,9 +5,14 @@ import com.gewuyou.shared.app.BaseForgeApplicationRunner;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @Slf4j
+@ComponentScan(basePackages = {
+        "com.gewuyou.scheduler",
+        "com.gewuyou.shared"
+})
 public class BaseForgeSchedulerApplication extends BaseForgeApplicationRunner {
 
     public static void main(String[] args) {
--- base-forge-search/src/main/java/com/gewuyou/search/BaseForgeSearchApplication.java
+++ base-forge-search/src/main/java/com/gewuyou/search/BaseForgeSearchApplication.java
@@ -1,11 +1,17 @@
 package com.gewuyou.search;
 
 
-import com.gewuyou.shared.app.BaseForgeApplicationRunner;
+
+import com.gewuyou.web.app.BaseForgeApplicationRunner;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 搜索服务启动类
+ *
+ * @author gewuyou
+ */
 @SpringBootApplication
 @Slf4j
 public class BaseForgeSearchApplication extends BaseForgeApplicationRunner {
