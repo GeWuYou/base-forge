34658bad12207bf23c1db949de198fed5b608043
gewuyou <1063891901@qq.com>
2025-01-24 00:40:12 +0800

    refactor(api): 重命名删除请求类并更新注释和错误信息

--- base-forge-starter/base-forge-web-spring-boot-starter/src/main/java/com/gewuyou/baseforge/entities/web/entity/DeleteByIdsReq.java
+++ base-forge-starter/base-forge-web-spring-boot-starter/src/main/java/com/gewuyou/baseforge/entities/web/entity/DeleteByIdsReq.java
@@ -1,4 +1,4 @@
-package com.gewuyou.baseforge.dictionary.request;
+package com.gewuyou.baseforge.entities.web.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotNull;
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 /**
- * 字典类别删除请求
+ * 根据id列表删除请求
  *
  * @author gewuyou
  * @since 2025-01-18 17:39:18
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DictionaryCategoryDeleteReq(@NotNull(message = "data.dictionaryCategory.ids.notNull") List<Long> ids) {
+public record DeleteByIdsReq(@NotNull(message = "web.delete.ids.notNull") List<Long> ids) {
 }
