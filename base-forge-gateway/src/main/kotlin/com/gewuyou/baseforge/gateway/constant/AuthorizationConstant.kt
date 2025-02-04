997d4a90a143ea94b5ef9e0ed0e4c365c8a447b9
gewuyou <1063891901@qq.com>
2025-02-04 09:19:56 +0800

    feat(gateway): ✨️新增用户反应式授权处理程序及配置

---
+++ base-forge-gateway/src/main/kotlin/com/gewuyou/baseforge/gateway/constant/AuthorizationConstant.kt
@@ -0,0 +1,15 @@
+package com.gewuyou.baseforge.gateway.constant
+
+/**
+ * 网关模块 常量
+ *
+ * @since 2025-01-25 09:37:13
+ * @author gewuyou
+ */
+data object GatewayConstant {
+    /**
+     * 权限缓存前缀
+     */
+    const val PERMISSION_CACHE_PREFIX = "auth:permission:"
+
+}
