950fef3fa50381893580eb73f9b71c04bd272767
gewuyou <1063891901@qq.com>
2025-02-04 09:21:27 +0800

    fix(data-dictionary): ️删除无用的DictionaryItemTranslationMapper.xml并修正导入路径

--- base-forge-data-dictionary/src/main/java/com/gewuyou/baseforge/dictionary/BaseForgeDataDictionaryApplication.java
+++ base-forge-data-dictionary/src/main/java/com/gewuyou/baseforge/dictionary/BaseForgeDataDictionaryApplication.java
@@ -1,6 +1,7 @@
 package com.gewuyou.baseforge.dictionary;
 
-import com.gewuyou.baseforge.entities.web.app.BaseForgeApplicationRunner;
+
+import com.gewuyou.baseforge.core.app.BaseForgeApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
