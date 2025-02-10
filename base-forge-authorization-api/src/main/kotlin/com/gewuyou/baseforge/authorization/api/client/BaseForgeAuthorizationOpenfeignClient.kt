9ff3ead6e0a44e1382fe9b95448591addeb55660
gewuyou <1063891901@qq.com>
2025-02-11 00:06:40 +0800

    refactor(api): 🧬️更新URL路径以支持版本控制

--- base-forge-authorization-api/src/main/kotlin/com/gewuyou/baseforge/authorization/api/client/BaseForgeAuthorizationOpenfeignClient.kt
+++ base-forge-authorization-api/src/main/kotlin/com/gewuyou/baseforge/authorization/api/client/BaseForgeAuthorizationOpenfeignClient.kt
@@ -16,33 +16,39 @@ import org.springframework.web.bind.annotation.PathVariable
 @FeignClient(name = "base-forge-authorization")
 interface BaseForgeAuthorizationOpenfeignClient {
     companion object{
-        private const val BASE_URL = "/auth/z"
-        const val USER_URL = BASE_URL.plus("/user")
-        const val PERMISSION_URL = BASE_URL.plus("/permission")
-        const val USER_ROLE_URL = BASE_URL.plus("/userRole")
+        private const val BASE_URL = "/api"
+        private const val V1 = "/v1"
+        private const val SERVICE_URL = "/auth/z"
+        private const val USER_URL = "/user"
+        private const val PERMISSION_URL ="/permission"
+        private const val USER_ROLE_URL ="/userRole"
+
+        private const val USER_URL_V1 = BASE_URL.plus(V1).plus(SERVICE_URL).plus(USER_URL)
+        private const val PERMISSION_URL_V1 = BASE_URL.plus(V1).plus(SERVICE_URL).plus(PERMISSION_URL)
+        private const val USER_ROLE_URL_V1 = BASE_URL.plus(V1).plus(SERVICE_URL).plus(USER_ROLE_URL)
     }
-    @GetMapping("$USER_URL/email/{email}")
+    @GetMapping("$USER_URL_V1/email/{email}")
     fun findByEmail(@PathVariable email: String): AuthUser?
 
-    @GetMapping("$USER_URL/phone/{phone}")
+    @GetMapping("$USER_URL_V1/phone/{phone}")
     fun findByPhone(@PathVariable phone: String): AuthUser?
 
-    @GetMapping("$USER_URL/username/{username}")
+    @GetMapping("$USER_URL_V1/username/{username}")
     fun findByUsername(@PathVariable username: String): AuthUser?
 
     /**
      * 根据key(前缀+url+method)获取权限列表
      */
-    @GetMapping("$PERMISSION_URL/{key}")
+    @GetMapping("$PERMISSION_URL_V1/{key}")
     fun getPermissionsByKey(@PathVariable key: String):Set<String>
 
     /**
      * 获取权限列表
      * @return 权限列表
      */
-    @GetMapping("$PERMISSION_URL/list")
+    @GetMapping("$PERMISSION_URL_V1/list")
     fun getAuthPermissionRoleDtoList(): List<AuthPermissionRoleDto>
 
-    @GetMapping("$USER_ROLE_URL/list/{userAuthId}")
+    @GetMapping("$USER_ROLE_URL_V1/list/{userAuthId}")
     fun getRoleByUserAuthId(@PathVariable userAuthId: Long): List<AuthRole>
 }
